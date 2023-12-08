b0e8a89a7f84a79286eaaf722f2c6b2f93508e50
tontonrodpenrai <142320807+tontonrodpenrai@users.noreply.github.com>
2023-12-08 21:54:57 +0700

    Update MainMenu.js

--- src/scene/MainMenu.js
+++ src/scene/MainMenu.js
@@ -11,16 +11,15 @@ class MainMenu extends Phaser.Scene {
         this.bgMusic
     }
     preload() {
-        this.load.image('background', "assets/PNG/elements/mainmenu.png")
-        this.load.image('start_button', "assets/PNG/elements/startbotton.PNG")
+        this.load.image('background', "assets/PNG/elements/mainmenu.png");
+        this.load.image('start_button', "assets/PNG/elements/startbotton.PNG");
         this.load.audio('bgMusic', "assets/Sound/watery_cave.mp3");
     }
     create() {
-        this.bg = this.add.image(this.sys.game.canvas.width/2,this.sys.game.canvas.height/2,'background')
-        this.button = this.add.image(this.sys.game.canvas.width/2, 470,'start_button').setScale( 0.15,0.15)
+        this.bg = this.add.image(0, 0, 'background').setOrigin(0,0)
+        this.button = this.add.image(650, 470,'start_button').setScale( 0.15,0.15)
         this.button.setInteractive();
         this.button.on("pointerdown",()=>{
-            // this.clickSound.play()
             this.scene.start("GameScene")
         })
 
@@ -37,8 +36,7 @@ class MainMenu extends Phaser.Scene {
         this.bgMusic.play(bgMusicConfig)
     }
 
-    update(delta, time) {
+    update(delta, time) {}
 
-    }
 }
 export default MainMenu;
